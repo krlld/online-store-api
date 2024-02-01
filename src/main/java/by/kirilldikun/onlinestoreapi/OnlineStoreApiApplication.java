78e15fddbdad00fbe5dba941b90589ffc11c1c5b
krlld <kirilldikun25@gmail.com>
2024-02-01 23:25:53 +0300

    first commit

---
+++ src/main/java/by/kirilldikun/onlinestoreapi/OnlineStoreApiApplication.java
@@ -0,0 +1,13 @@
+package by.kirilldikun.onlinestoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/kirilldikun/onlinestoreapi/OnlineStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package by.kirilldikun.onlinestoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
