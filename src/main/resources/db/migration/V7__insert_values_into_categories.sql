77fd5d45e5b585bd05b37272dda9dac5c199fbba
krlld <kirilldikun25@gmail.com>
2024-03-10 00:47:20 +0300

    feat: migration with categories values

---
+++ src/main/resources/db/migration/V7__insert_values_into_categories.sql
@@ -0,0 +1,9 @@
+INSERT INTO categories(name)
+VALUES ('Telephone'),
+       ('Videocard'),
+       ('Keyboard'),
+       ('Computer mouse'),
+       ('Monitor'),
+       ('Notepad'),
+       ('TV'),
+       ('Watch');
