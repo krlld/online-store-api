9538c06eefbe7f7ac957018963567b25250be458
krlld <kirilldikun25@gmail.com>
2024-03-10 00:47:40 +0300

    feat: migration with products and images values

---
+++ src/main/resources/db/migration/V8__insert_values_into_products.sql
@@ -0,0 +1,19 @@
+INSERT INTO products (name, price, description, category_id)
+VALUES ('Смартфон Apple iPhone 15 Plus 512GB (голубой)', 7916.76,
+        'Apple iOS, экран 6.7" OLED (1290x2796) 60 Гц, Apple A16 Bionic, ОЗУ 6 ГБ, память 512 ГБ, камера 48 Мп, аккумулятор 4383 мАч, 1 SIM (nano-SIM/eSIM), влагозащита IP68',
+        1),
+       ('Смартфон Samsung Galaxy Z Fold5 SM-F946B/DS 12GB/1TB (голубой)', 6313,
+        'Android, экран 7.6" AMOLED (1812x2176) 120 Гц, Qualcomm Snapdragon 8 Gen2 SM8550, ОЗУ 12 ГБ, память 1 ТБ, камера 50 Мп, аккумулятор 4400 мАч, 2 SIM (nano-SIM/eSIM), влагозащита IPX8',
+        1),
+       ('Смартфон Samsung Galaxy S24 Ultra SM-S9280 12GB/1TB (титановый фиолетовый)', 5999,
+        'Android, экран 6.8" AMOLED (1440x3120) 120 Гц, Qualcomm Snapdragon 8 Gen 3, ОЗУ 12 ГБ, память 1 ТБ, камера 200 Мп, аккумулятор 5000 мАч, 2 SIM (nano-SIM), влагозащита IP68',
+        1),
+       ('Смартфон Google Pixel 8 Pro 12GB/512GB (залив)', 4890,
+        'Android, экран 6.7" OLED (1344x2992) 120 Гц, Google Tensor G3, ОЗУ 12 ГБ, память 512 ГБ, камера 50 Мп, аккумулятор 5050 мАч, 1 SIM (nano-SIM/eSIM), влагозащита IP68',
+        1);
+
+INSERT INTO images (url, product_id)
+VALUES ('https://content2.onliner.by/catalog/device/main/91c77260bf1e69ab91073077910bae1a.jpg', 1),
+       ('https://content2.onliner.by/catalog/device/main/e8cfc41047a3cca70fa0b735ecd27921.jpeg', 2),
+       ('https://content2.onliner.by/catalog/device/main/863b823dd2f003a9808938db22296826.jpg', 3),
+       ('https://content2.onliner.by/catalog/device/main/8622657ef008e8d6ae91517aa6133b9c.jpg', 4);
